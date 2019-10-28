ca2dc8a3cdc0c5a802712bf886f7811b82ff54cb
Erik_N <Eriknick24@gmail.com>
2019-10-28 00:49:27 -0400

    Home page for project

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+#new home urls file
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home-home'),
+	path('about/', views.about, name='home-about'),
+]
---
+++ home/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.	
+
+members = [
+	{
+		'name':			'Corey',
+		'content':		'Senior at the University of South Carolina working on a BS in Computer Science. \n I have an Associates degree in Mechanical Engineering from Midlands Technical College.'
+	},
+	{
+		'name':			'Erik',
+		'content':		'Hey, my name is Erik, and Im a junior in Computer Science.'
+	},
+	{
+		'name':			'Itay',
+		'content':		'Hello, my name is Itay Goldfaden and I am a senior'
+	},
+	{
+		'name':			'James',
+		'content':		'Hey this james testing.'
+	},
+	{
+		'name':			'Matt',
+		'content':		'Hello  my name is matt and i am a senior CIS major.'
+	}
+]
+
+def home(request):
+	return render(request, 'home/home.html', {'title': 'Home Page'})
+	
+def about(request):
+	context = {
+		'members': members
+	}
+	return render(request, 'home/about.html', context)
+
