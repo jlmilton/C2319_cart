c3d8462d14c6fb7669b898bd4313fc76805f728f
Itay Goldfaden <47089377+duks500@users.noreply.github.com>
2019-10-29 12:05:11 -0400

    Update settings.py

--- c2319/settings.py
+++ c2319/settings.py
@@ -72,44 +72,44 @@ TEMPLATES = [
 WSGI_APPLICATION = 'c2319.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+ #Database
+ #https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 # [START db_setup]
-if os.getenv('GAE_APPLICATION', None):
+#if os.getenv('GAE_APPLICATION', None):
     # Running on production App Engine, so connect to Google Cloud SQL using
     # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/ethereal-argon-257423:us-central1:c-2319',
-            'USER': 'admin',
-            'PASSWORD': 'admin1234',
-            'NAME': 'try',
-        }
-    }
-else:
+    #DATABASES = {
+        #'default': {
+           # 'ENGINE': 'django.db.backends.mysql',
+        #    'HOST': '/cloudsql/ethereal-argon-257423:us-central1:c-2319',
+        #    'USER': 'admin',
+      #      'PASSWORD': 'admin1234',
+     #       'NAME': 'try',
+    #    }
+ #   }
+#else:
     # Running locally so connect to either a local MySQL instance or connect
     # to Cloud SQL via the proxy.  To start the proxy via command line:
     #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
     # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'NAME': 'try',
-            'USER': 'admin',
-            'PASSWORD': 'admin1234',
-        }
-    }
+   # DATABASES = {
+      #  'default': {
+      #      'ENGINE': 'django.db.backends.mysql',
+      #      'HOST': '127.0.0.1',
+      #      'PORT': '3306',
+      #      'NAME': 'try',
+      #      'USER': 'admin',
+      #      'PASSWORD': 'admin1234',
+     #   }
+  #  }
 # [END db_setup]
 
 
