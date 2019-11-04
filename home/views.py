34fd8449d8dd3a46fe26f5c508cb4bb5343c246b
Corey Brown <cbsoccer02@aol.com>
2019-11-04 11:00:13 -0500

    Clean up

--- home/views.py
+++ home/views.py
@@ -4,36 +4,10 @@ from .models import About
 
 # Create your views here.	
 
-# members = [
-# 	{
-# 		'name':			'Corey',
-# 		'content':		'Senior at the University of South Carolina working on a BS in Computer Science. \n I have an Associates degree in Mechanical Engineering from Midlands Technical College.'
-# 	},
-# 	{
-# 		'name':			'Erik',
-# 		'content':		'Hey, my name is Erik, and Im a junior in Computer Science.'
-# 	},
-# 	{
-# 		'name':			'Itay',
-# 		'content':		'Hello, my name is Itay Goldfaden and I am a senior'
-# 	},
-# 	{
-# 		'name':			'James',
-# 		'content':		'Hey this james testing.'
-# 	},
-# 	{
-# 		'name':			'Matt',
-# 		'content':		'Hello  my name is matt and I am a senior CIS major.'
-# 	}
-# ]
-
 def home(request):
 	return render(request, 'home/home.html', {'title': 'Home Page'})
 	
 def about(request):
-	# context = {
-	# 	'members': members
-	# }
 	return render(request = request,
 				  template_name='home/about.html',
 				  context = {'about': About.objects.all})
