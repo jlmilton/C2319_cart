ae07458d4b335c70ce7b8b465a8908335e222c3b
itay@email.sc.edu <itay@email.sc.edu>
2019-11-10 23:44:30 -0500

    create user and log in use

--- c2319/urls.py
+++ c2319/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_page.urls')),
     path('post/', include('posts.urls')),
+    path('accounts/', include('accounts.urls')), # new
     path('accounts/', include('django.contrib.auth.urls')), # new
-    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
