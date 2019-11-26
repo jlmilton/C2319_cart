46a6ffe88d9504e7d2ec6e704d8e94ef2942d565
Corey Brown <cbsoccer02@aol.com>
2019-11-26 14:18:09 -0500

    changed back to lite

--- c2319/settings.py
+++ c2319/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'c2319.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.mysql',
-             'HOST': '127.0.0.1',
-             'PORT': '3306',
-            #  'NAME': 'c_2319',
-             'NAME': os.path.join(BASE_DIR, 'db.c_2319'),
-             'USER': 'root',
-             'PASSWORD': 'root',
-             'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
-         }
-     }
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+    #      'default': {
+    #          'ENGINE': 'django.db.backends.mysql',
+    #          'HOST': '127.0.0.1',
+    #          'PORT': '3306',
+    #         #  'NAME': 'c_2319',
+    #          'NAME': os.path.join(BASE_DIR, 'db.c_2319'),
+    #          'USER': 'root',
+    #          'PASSWORD': 'root',
+    #          'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
+    #      }
+    #  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
