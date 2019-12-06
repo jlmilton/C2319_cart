de94e421f3a270f5fbc29c700b8cc5ebd3e3c48c
Corey Brown <cbsoccer02@aol.com>
2019-12-05 20:54:38 -0500

    host change

--- c2319/settings.py
+++ c2319/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         # 'HOST': '',
         # 'PORT': '',
         'PASSWORD': 'root',
-        'HOST': '2319.pythonanywhere.com',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
