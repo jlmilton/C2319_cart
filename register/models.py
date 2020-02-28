86c5b0d45ae919d7f970854be9fad40aa1d52f97
itay@email.sc.edu <itay@email.sc.edu>
2020-02-27 21:06:55 -0500

    issue 57 and issue 61

--- register/models.py
+++ register/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+JOB = (
+    (0,"Undergraduate Student"),
+    (1,"Graduate Student"),
+    (2,"Faculty Member"),
+    (3,"Staff"),
+)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    location = models.CharField(max_length=30)
+    age = models.IntegerField()
+    occupation = models.IntegerField(choices=JOB , default = 4)
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+
+
+    def __str__(self):
+        return self.user.username
--- register/views.py
+++ register/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from .forms import UserProfileForm
 
 # Create your views here.
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
-        if form.is_valid():
-            form.save()
+        profile_form = UserProfileForm(response.POST)
+        if form.is_valid() and profile_form.is_valid():
+            # form.save()
+            user = form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
         return redirect('/')
     else:
         form = RegisterForm()
-    return render(response, 'register/register.html' , {'form':form})
+        profile_form = UserProfileForm()
+    context = {'form' : form, 'profile_form' : profile_form}
+    return render(response, 'register/register.html' , context)
