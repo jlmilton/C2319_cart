78160cff92542589a1442aca323ac5504f3f2880
duks500 <itay@email.sc.edu>
2020-04-21 08:08:17 -0400

    milestone#17

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('milestone_17/', views.milestone_17, name='milestone_17'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,9 +8,11 @@ from django.contrib import messages
 def home(request):
     return render(request, '../templates/home.html', {'title': 'Home'})
 
-
 def about(request):
     return render(request, '../templates/about.html', {'title': 'About'})
+
+def milestone_17(request):
+    return render(request, '../templates/milestone_17.html', {'title': 'Milestone 17'})
 
 def forsale(request):
     return render(request, '../templates/forsale.html', {'title': 'For Sale'})
